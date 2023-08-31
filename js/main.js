29887f31b19a91deafc89000d081c1ce5b9a16de
oivatO-soD-sotnaS <116353611+oivatO-soD-sotnaS@users.noreply.github.com>
2023-08-31 19:52:12 -0300

    Melhorias na legibilidade do código#1
    Melhorias nos nomes das classes

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ const xColor = "#F96167";
 const oColor = "#F9E795";
 const imageX = "../images/noun-x-2126085.png";
 const imageO = "../images/noun-circle-1157067.png";
-const ticTacToe = document.querySelector(".tic-tac-toe");
-const box0 = document.getElementById("0");
-const box1 = document.getElementById("1");
-const box2 = document.getElementById("2");
-const box3 = document.getElementById("3");
-const box4 = document.getElementById("4");
-const box5 = document.getElementById("5");
-const box6 = document.getElementById("6");
-const box7 = document.getElementById("7");
-const box8 = document.getElementById("8");
+const jogoDaVelha = document.querySelector(".jogo-da-velha");
+const casa0 = document.getElementById("0");
+const casa1 = document.getElementById("1");
+const casa2 = document.getElementById("2");
+const casa3 = document.getElementById("3");
+const casa4 = document.getElementById("4");
+const casa5 = document.getElementById("5");
+const casa6 = document.getElementById("6");
+const casa7 = document.getElementById("7");
+const casa8 = document.getElementById("8");
 let pontuacaoX = document.getElementById("pontuacaoX");
 let pontuacaoO = document.getElementById("pontuacaoO");
 let jogador = "X";
@@ -21,32 +21,32 @@ let jogador = "X";
 
 
 //Detecta o click em qualquer uma das 9 caixas do jogo
-box0.addEventListener("click", () => {
-    colocaSimbolo(box0);
+casa0.addEventListener("click", () => {
+    colocaSimbolo(casa0);
 });
-box1.addEventListener("click", () => {
-    colocaSimbolo(box1);
+casa1.addEventListener("click", () => {
+    colocaSimbolo(casa1);
 });
-box2.addEventListener("click", () => {
-    colocaSimbolo(box2);
+casa2.addEventListener("click", () => {
+    colocaSimbolo(casa2);
 });
-box3.addEventListener("click", () => {
-    colocaSimbolo(box3);
+casa3.addEventListener("click", () => {
+    colocaSimbolo(casa3);
 });
-box4.addEventListener("click", () => {
-    colocaSimbolo(box4);
+casa4.addEventListener("click", () => {
+    colocaSimbolo(casa4);
 });
-box5.addEventListener("click", () => {
-    colocaSimbolo(box5);
+casa5.addEventListener("click", () => {
+    colocaSimbolo(casa5);
 });
-box6.addEventListener("click", () => {
-    colocaSimbolo(box6);
+casa6.addEventListener("click", () => {
+    colocaSimbolo(casa6);
 });
-box7.addEventListener("click", () => {
-    colocaSimbolo(box7);
+casa7.addEventListener("click", () => {
+    colocaSimbolo(casa7);
 });
-box8.addEventListener("click", () => {
-    colocaSimbolo(box8);
+casa8.addEventListener("click", () => {
+    colocaSimbolo(casa8);
 });
 
 //Criação de todas as funções utilizadas no programa
@@ -70,15 +70,18 @@ function checarVitoria(){
     vitória após cada rodada para detectar os seguintes casos:
     Vitória de qualquer um dos jogadores ou empate.
     */
-    const filhosTicTacToe = ticTacToe.children;
-    const linha1 = [filhosTicTacToe[0].dataset.marked,filhosTicTacToe[1].dataset.marked,filhosTicTacToe[2].dataset.marked];
-    const linha2 = [filhosTicTacToe[3].dataset.marked,filhosTicTacToe[4].dataset.marked,filhosTicTacToe[5].dataset.marked];
-    const linha3 = [filhosTicTacToe[6].dataset.marked,filhosTicTacToe[7].dataset.marked,filhosTicTacToe[8].dataset.marked];
-    const coluna1 = [filhosTicTacToe[0].dataset.marked,filhosTicTacToe[3].dataset.marked,filhosTicTacToe[6].dataset.marked];
-    const coluna2 = [filhosTicTacToe[1].dataset.marked,filhosTicTacToe[4].dataset.marked,filhosTicTacToe[7].dataset.marked];
-    const coluna3 = [filhosTicTacToe[2].dataset.marked,filhosTicTacToe[5].dataset.marked,filhosTicTacToe[8].dataset.marked];
-    const diagonal1 = [filhosTicTacToe[0].dataset.marked,filhosTicTacToe[4].dataset.marked,filhosTicTacToe[8].dataset.marked];
-    const diagonal2 = [filhosTicTacToe[2].dataset.marked,filhosTicTacToe[4].dataset.marked,filhosTicTacToe[6].dataset.marked];
+    const casasJogoDaVelha = jogoDaVelha.children;
+
+    const linha1 = [casasJogoDaVelha[0].dataset.marked,casasJogoDaVelha[1].dataset.marked,casasJogoDaVelha[2].dataset.marked];
+    const linha2 = [casasJogoDaVelha[3].dataset.marked,casasJogoDaVelha[4].dataset.marked,casasJogoDaVelha[5].dataset.marked];
+    const linha3 = [casasJogoDaVelha[6].dataset.marked,casasJogoDaVelha[7].dataset.marked,casasJogoDaVelha[8].dataset.marked];
+
+    const coluna1 = [casasJogoDaVelha[0].dataset.marked,casasJogoDaVelha[3].dataset.marked,casasJogoDaVelha[6].dataset.marked];
+    const coluna2 = [casasJogoDaVelha[1].dataset.marked,casasJogoDaVelha[4].dataset.marked,casasJogoDaVelha[7].dataset.marked];
+    const coluna3 = [casasJogoDaVelha[2].dataset.marked,casasJogoDaVelha[5].dataset.marked,casasJogoDaVelha[8].dataset.marked];
+
+    const diagonal1 = [casasJogoDaVelha[0].dataset.marked,casasJogoDaVelha[4].dataset.marked,casasJogoDaVelha[8].dataset.marked];
+    const diagonal2 = [casasJogoDaVelha[2].dataset.marked,casasJogoDaVelha[4].dataset.marked,casasJogoDaVelha[6].dataset.marked];
 
     if(allEqual(linha1) && linha1[0] != "false"){
         aumentaPontuacao(linha1[0]);
@@ -115,26 +118,26 @@ function checarVitoria(){
     
     
 }
-const allEqual = arr => arr.every(val => val === arr[0]);
 
-function colocaSimbolo(boxId){
+
+function colocaSimbolo(id_casa){
     /*
     Esta função irá colocar o simbolo 
     apropriado na caixa clickada.
     */
-    if(boxId.dataset.marked != "false"){
+    if(id_casa.dataset.marked != "false"){
         return;
     }
     if(jogador === "X"){
-        boxId.dataset.marked = "X";
-        boxId.style.backgroundColor = xColor;
-        boxId.childNodes[0].style.opacity = 1;
+        id_casa.dataset.marked = "X";
+        id_casa.style.backgroundColor = xColor;
+        id_casa.childNodes[0].style.opacity = 1;
         mudaImagen(imageO);
         jogador = "O";
     }else{
-        boxId.dataset.marked = "O";
-        boxId.style.backgroundColor = oColor;
-        boxId.childNodes[0].style.opacity = 1;
+        id_casa.dataset.marked = "O";
+        id_casa.style.backgroundColor = oColor;
+        id_casa.childNodes[0].style.opacity = 1;
         mudaImagen(imageX);
         jogador = "X";
     }
@@ -155,10 +158,12 @@ function mudaImagen(imagem){
     /*
     Está função irá trocar as imagens das caixas desmarcadas
     */
-    const filhosTicTacToe = ticTacToe.children;
-    for(let index = 0 ; index < filhosTicTacToe.length; ++index){
-        if(filhosTicTacToe[index].dataset.marked == "false"){
-            filhosTicTacToe[index].childNodes[0].src = imagem
+    const casasJogoDaVelha = jogoDaVelha.children;
+    for(let index = 0 ; index < casasJogoDaVelha.length; ++index){
+        if(casasJogoDaVelha[index].dataset.marked == "false"){
+            casasJogoDaVelha[index].childNodes[0].src = imagem
         }
     }
 }
+
+const allEqual = arr => arr.every(val => val === arr[0]);
